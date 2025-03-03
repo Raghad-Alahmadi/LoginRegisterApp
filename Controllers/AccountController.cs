82ade12da50867bda4810e6a24c78be0f3ebf9f9
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-03-03 20:55:57 +0300

    Initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class AccountController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AccountController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: /Account/Register
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    // POST: /Account/Register
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Login));
+        }
+        return View(user);
+    }
+
+    // GET: /Account/Login
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    // POST: /Account/Login
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(string username, string password)
+    {
+        var user = await _context.Users
+                                  .FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+        if (user != null)
+        {
+            // User logged in successfully, redirect to home or another page
+            return RedirectToAction("Index", "Home");
+        }
+        ModelState.AddModelError("", "Invalid login attempt.");
+        return View();
+    }
+}
